64451905271de4ae20d8bebfe3df9deb46488b6d
soni-shashan <lumbhanishashan1510@gmail.com>
2024-03-25 13:00:27 +0530

    ADD TAB BAR FOR ADD PRODUCT,REMOVE PRODUCT,UPDATE PRODUCT,SEARCH PRODUCT,DISPLAY INVENTORY

--- INVENTORY_MANAGEMENT_SYSTEM/Main.h
+++ INVENTORY_MANAGEMENT_SYSTEM/Main.h
@@ -14,6 +14,12 @@ namespace INVENTORYMANAGEMENTSYSTEM {
 	/// </summary>
 	public ref class Main : public System::Windows::Forms::Form
 	{
+	private: System::Windows::Forms::TabControl^ tabControl1;
+	private: System::Windows::Forms::TabPage^ tabPage1;
+	private: System::Windows::Forms::TabPage^ tabPage2;
+	private: System::Windows::Forms::TabPage^ tabPage3;
+	private: System::Windows::Forms::TabPage^ tabPage4;
+	private: System::Windows::Forms::TabPage^ tabPage5;
 	public:
 		INVENTORYMANAGEMENTSYSTEM::excel^ Excel = gcnew INVENTORYMANAGEMENTSYSTEM::excel("INVENTORY.xlsx");
 		const char* _filename = "INVENTORY.xlsx";
@@ -48,13 +54,91 @@ namespace INVENTORYMANAGEMENTSYSTEM {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage3 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage4 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage5 = (gcnew System::Windows::Forms::TabPage());
+			this->tabControl1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this->tabControl1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tabControl1->Controls->Add(this->tabPage1);
+			this->tabControl1->Controls->Add(this->tabPage2);
+			this->tabControl1->Controls->Add(this->tabPage3);
+			this->tabControl1->Controls->Add(this->tabPage4);
+			this->tabControl1->Controls->Add(this->tabPage5);
+			this->tabControl1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->tabControl1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->tabControl1->Location = System::Drawing::Point(12, 12);
+			this->tabControl1->Name = L"tabControl1";
+			this->tabControl1->SelectedIndex = 0;
+			this->tabControl1->Size = System::Drawing::Size(1758, 945);
+			this->tabControl1->TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->Location = System::Drawing::Point(4, 29);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage1->Size = System::Drawing::Size(1750, 912);
+			this->tabPage1->TabIndex = 0;
+			this->tabPage1->Text = L"ADD PRODUCT";
+			this->tabPage1->UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Location = System::Drawing::Point(4, 29);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(1750, 912);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"REMOVE PRODUCT";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// tabPage3
+			// 
+			this->tabPage3->Location = System::Drawing::Point(4, 29);
+			this->tabPage3->Name = L"tabPage3";
+			this->tabPage3->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage3->Size = System::Drawing::Size(1750, 912);
+			this->tabPage3->TabIndex = 2;
+			this->tabPage3->Text = L"UPDATE PRODUCT";
+			this->tabPage3->UseVisualStyleBackColor = true;
+			this->tabPage3->Click += gcnew System::EventHandler(this, &Main::tabPage3_Click);
+			// 
+			// tabPage4
+			// 
+			this->tabPage4->Location = System::Drawing::Point(4, 25);
+			this->tabPage4->Name = L"tabPage4";
+			this->tabPage4->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage4->Size = System::Drawing::Size(1750, 916);
+			this->tabPage4->TabIndex = 3;
+			this->tabPage4->Text = L"SEARCH PRODUCT";
+			this->tabPage4->UseVisualStyleBackColor = true;
+			// 
+			// tabPage5
+			// 
+			this->tabPage5->Location = System::Drawing::Point(4, 25);
+			this->tabPage5->Name = L"tabPage5";
+			this->tabPage5->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage5->Size = System::Drawing::Size(1750, 916);
+			this->tabPage5->TabIndex = 4;
+			this->tabPage5->Text = L"DISPLAY INVENTORY";
+			this->tabPage5->UseVisualStyleBackColor = true;
 			// 
 			// Main
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1062, 1033);
+			this->ClientSize = System::Drawing::Size(1782, 1033);
+			this->Controls->Add(this->tabControl1);
 			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->MaximizeBox = false;
 			this->Name = L"Main";
@@ -62,11 +146,14 @@ namespace INVENTORYMANAGEMENTSYSTEM {
 			this->Text = L"INVENTORY MANAGEMENT SYSTEM";
 			this->WindowState = System::Windows::Forms::FormWindowState::Maximized;
 			this->Load += gcnew System::EventHandler(this, &Main::Main_Load);
+			this->tabControl1->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	private: System::Void Main_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void tabPage3_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
